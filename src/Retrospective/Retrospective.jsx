fcf795ba2b151bdb244e2cf7f76b959c9fc1dec5
Ksendel <oksana.karachun14@gmail.com>
2021-03-03 22:32:20 +0200

    first commit

---
+++ src/Retrospective/Retrospective.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import CardsColumn from "./CardsColumn/CardsColumn";
+
+class Retrospective extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const green = {
+            backgroundColor: '#b7ffa6'
+        }
+        const red = {
+            backgroundColor: '#ff8080'
+        }
+
+        const blue = {
+            backgroundColor: '#7fddff'
+        }
+        return <div className='Retrospective'>
+            <div className='wrapper'>
+            <header><h1>Retrospective task</h1></header>
+                <div className='columns'>
+                    <CardsColumn title={'Good things'} colorCard={green}/>
+                    <CardsColumn title={'Bad things'} colorCard={red}/>
+                    <CardsColumn title={'Actions items'} colorCard={blue}/>
+                </div>
+            </div>
+        </div>
+    }
+}
+
+export default Retrospective
